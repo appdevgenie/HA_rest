b4a3d32b4d8ac95484f8f69c1f9777922d179bd1
appdevgenie <appdevgenie@gmail.com>
2021-08-28 07:40:47 +0200

    update recyclerview adapter

--- app/src/main/java/com/example/harest/adapter/StatesListAdapter.java
+++ app/src/main/java/com/example/harest/adapter/StatesListAdapter.java
@@ -1,35 +1,61 @@
 package com.example.harest.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.harest.R;
+import com.example.harest.model.StateModel;
+
+import java.util.ArrayList;
+
 public class StatesListAdapter extends RecyclerView.Adapter<StatesListAdapter.StatesViewHolder> {
 
+    private Context context;
+    private ArrayList<StateModel> stateModelArrayList;
+
+    public StatesListAdapter(Context context, ArrayList<StateModel> stateModelArrayList) {
+        this.context = context;
+        this.stateModelArrayList = stateModelArrayList;
+    }
 
     @NonNull
     @Override
     public StatesListAdapter.StatesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_state, parent, false);
+        return new StatesViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StatesListAdapter.StatesViewHolder holder, int position) {
 
+        holder.entity_id.setText(stateModelArrayList.get(position).getEntity_id());
+        holder.last_changed.setText(stateModelArrayList.get(position).getLast_changed());
+        holder.state.setText(stateModelArrayList.get(position).getState());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return stateModelArrayList.size();
     }
 
     public class StatesViewHolder extends RecyclerView.ViewHolder{
 
+        private TextView entity_id;
+        private TextView last_changed;
+        private TextView state;
 
         public StatesViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            entity_id = itemView.findViewById(R.id.text_item_entity_id);
+            last_changed = itemView.findViewById(R.id.text_item_last_changed);
+            state = itemView.findViewById(R.id.text_item_state);
         }
     }
 }
